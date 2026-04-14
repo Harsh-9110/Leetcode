6131dc387bab07cf063f05397c934704c8d474f6
Harsh <harsh09official@gmail.com>
2026-04-14 08:09:03 +0530

    Time: 4 ms (42.38%) | Memory: 10.7 MB (91.16%) - LeetSync

---
+++ 71-simplify-path/simplify-path.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> st;
+        string curr;
+
+        for (int i = 0; i <= path.size(); i++) {
+            if (i == path.size() || path[i] == '/') {
+                if (curr == "..") {
+                    if (!st.empty()) st.pop_back();
+                } 
+                else if (!curr.empty() && curr != ".") {
+                    st.push_back(curr);
+                }
+                curr = "";
+            } else {
+                curr += path[i];
+            }
+        }
+
+        string res = "/";
+        for (int i = 0; i < st.size(); i++) {
+            res += st[i];
+            if (i != st.size() - 1) res += "/";
+        }
+
+        return res;
+    }
+};
