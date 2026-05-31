e860173087afdec534e5f1c2c71b6e5eae60e5cf
Harsh <harsh09official@gmail.com>
2026-06-01 05:19:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (73.18%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+
+        string st;
+
+        for(char digit : num) {
+
+            while(!st.empty() &&
+                  k > 0 &&
+                  st.back() > digit) {
+
+                st.pop_back();
+                k--;
+            }
+
+            st.push_back(digit);
+        }
+
+        // If removals still left
+        while(k > 0) {
+
+            st.pop_back();
+            k--;
+        }
+
+        // Remove leading zeros
+        int i = 0;
+
+        while(i < st.size() &&
+              st[i] == '0') {
+
+            i++;
+        }
+
+        string ans = st.substr(i);
+
+        return ans.empty()
+               ? "0"
+               : ans;
+    }
+};
