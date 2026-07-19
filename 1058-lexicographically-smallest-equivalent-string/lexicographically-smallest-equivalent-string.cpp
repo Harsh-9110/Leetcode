18d5a41e6ff69d5a72bab85cfed3f64c3bc6c55d
Harsh <harsh09official@gmail.com>
2026-07-19 23:45:25 +0530

    Time: 3 ms (39.19%) | Memory: 8.9 MB (74.68%) - LeetSync

---
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> parent;
+
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    void unite(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+
+        if (px == py)
+            return;
+
+        // Always keep the lexicographically smaller character as parent
+        if (px < py)
+            parent[py] = px;
+        else
+            parent[px] = py;
+    }
+
+    string smallestEquivalentString(string s1, string s2, string baseStr) {
+        parent.resize(26);
+
+        for (int i = 0; i < 26; i++)
+            parent[i] = i;
+
+        for (int i = 0; i < s1.size(); i++)
+            unite(s1[i] - 'a', s2[i] - 'a');
+
+        for (char &c : baseStr)
+            c = find(c - 'a') + 'a';
+
+        return baseStr;
+    }
+};
